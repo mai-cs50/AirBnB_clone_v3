214fae300fa656ebbbe48d88296e0cee20cfccd8
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-03 06:44:14 -0700

    s

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,6 @@
 -- prepares a MySQL server for the project --
-CREAT DATABASE IF NOT EXISTS hbnb_dev_db:
-CREAT USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-USE hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db:
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON 'hbnb_dev_db' .* TO 'hbnb_dev'@'localhost';
-USE performance_schema;
 GRANT SELECT ON 'performance_schema' .* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
-SET FOREIGN_KEY_CHECKS=1;
